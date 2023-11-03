9094d7e957adea77ee8533bbc38eef5e32a41e3a
vishalpukale <vishalpukale12@.com>
2023-11-03 14:59:02 +0530

    hi

--- 60-25_Oct-2023 Linked List Problem/1one.js
+++ 60-25_Oct-2023 Linked List Problem/1one.js
@@ -1,41 +1,32 @@
-class Node{
-    constructor(){
+var Node = /** @class */ (function () {
+    function Node() {
         this.data = 0;
         this.next = null;
     }
-}
-
-let head;
-
-function insert(root, item){
+    return Node;
+}());
+var head;
+function insert(root, item) {
     var temp = new Node();
     temp.data = item;
     temp.next = root;
     root = temp;
     return root;
 }
-
-
-
-function print(head){
-    while(head != null){
+function print(head) {
+    while (head != null) {
         console.log(head.data + " ");
         head = head.next;
     }
 }
-
-
-function arrToList(arr){
-    let n = arr.length;
+function arrToList(arr) {
+    var n = arr.length;
     head = null;
-    for(let i = n-1; i >= 0; i--){
+    for (var i = n - 1; i >= 0; i--) {
         head = insert(head, arr[i]);
     }
     return head;
 }
-
-let arr = [1,2,3,4];
-
-
+var arr = [1, 2, 3, 4];
 head = arrToList(arr);
 console.log(head);
