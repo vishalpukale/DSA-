f91b77736460df53de2288ec5feabba78f027375
vishalpukale <vishalpukale12@.com>
2023-11-04 16:19:01 +0530

    link list

--- 61-29_Oct-2023 Live Class - Linked Lists 1/1intro.js
+++ 61-29_Oct-2023 Live Class - Linked Lists 1/1intro.js
@@ -48,6 +48,108 @@ var LinkedList = /** @class */ (function () {
             }
             this.size++;
         };
+        //remove first 
+        this.removeFirst = function () {
+            //if the link list is empty
+            if (this.size == 0) {
+                console.log("empty link list");
+                return -1;
+            }
+            var returnVal = this.head.val;
+            //If there is a single node
+            if (this.head.next == null) {
+                this.head = null;
+                this.tail = null;
+            }
+            else {
+                this.head = this.head.next;
+            }
+            this.size--;
+            return returnVal;
+        };
+        //remove last
+        this.removeLast = function () {
+            //if the link list is empty
+            if (this.size == 0) {
+                console.log("empty link list");
+                return -1;
+            }
+            var returnVal = this.tail.val;
+            if (this.head.next == null) {
+                this.head = null;
+                this.tail = null;
+            }
+            else {
+                var temp = this.head;
+                while (temp.next != this.tail) {
+                    temp = temp.next;
+                }
+                this.tail = temp;
+                temp.next = null;
+            }
+            this.size--;
+            return returnVal;
+        };
+        //Get an element from the index  
+        //Simply we are implementing the indexing in linked list    
+        this.getAt = function (index) {
+            if (index < 0 || index >= this.size) {
+                console.log("Index out of range");
+                return null;
+            }
+            var temp = this.head;
+            var currentIndex = 0;
+            while (currentIndex != index) {
+                currentIndex++;
+                temp = temp.next;
+            }
+            return temp;
+        };
+        //Add a node at a index 
+        this.addAt = function (index, val) {
+            //invalid case
+            if (index < 0 || index > this.size) {
+                console.log("Index out of range");
+                return;
+            }
+            else if (index == 0) {
+                this.addFirst(val);
+                return;
+            }
+            else if (index == this.size) {
+                this.addLast(val);
+                return;
+            }
+            //add in the middle of the linked list
+            else {
+                var nn = new ListNode(val);
+                var temp = this.getAt(index - 1);
+                nn.next = temp.next;
+                temp.next = nn;
+                this.size++;
+            }
+        };
+        //remove the node from the list
+        this.removeAt = function (index) {
+            //invalid case
+            if (index < 0 || index >= this.size) {
+                console.log("Index out of range");
+                return -1;
+            } //remove first
+            else if (index == 0) {
+                return this.removeFirst();
+            } //remove last
+            else if (index == this.size - 1) {
+                return this.removeLast();
+            }
+            else {
+                var temp = this.getAt(index - 1);
+                var returnVal = temp.next.val;
+                temp.next = temp.next.next;
+                this.size--;
+                return returnVal;
+            }
+        };
         this.head = null;
         this.tail = null;
         this.size = 0;
@@ -61,7 +163,13 @@ ll.addLast(40);
 ll.addLast(30);
 ll.addLast(20);
 ll.addLast(10);
-ll.addFirst(50);
 ll.addFirst(10000);
-console.log(ll.getSize());
+ll.display();
+console.log("We have removed the first node which is - " + ll.removeFirst());
+ll.display();
+console.log(ll.getAt(4).val);
+ll.display();
+ll.addAt(2, 5000);
+ll.display();
+ll.removeAt(0);
 ll.display();
--- 61-29_Oct-2023 Live Class - Linked Lists 1/1intro.ts
+++ 61-29_Oct-2023 Live Class - Linked Lists 1/1intro.ts
@@ -65,7 +65,123 @@ class LinkedList{
         }
         this.size++;
     }
+
+
+    //remove first 
+    removeFirst = function(): number{
+        //if the link list is empty
+        if(this.size == 0){
+            console.log("empty link list");
+            return -1;
+        }
+
+        let returnVal: number = this.head.val;
+
+        //If there is a single node
+        if(this.head.next == null){
+            this.head = null;
+            this.tail = null;
+        } else{
+            this.head = this.head.next;
+        }
+        this.size--;
+        return returnVal;
+    }
+
+
+    //remove last
+    removeLast = function(): number{ // TC : O(N)
+        //if the link list is empty
+        if(this.size == 0){
+            console.log("empty link list");
+            return -1;
+        }
+
+        let returnVal = this.tail.val;
+        if(this.head.next == null){
+            this.head = null;
+            this.tail = null;
+        }else{
+            let temp = this.head;
+            while(temp.next != this.tail){
+                temp = temp.next;
+            }
+            this.tail = temp;
+            temp.next = null;   
+        }
+        this.size--;
+        return returnVal;
+    }
+
+
+    //Get an element from the index  
+    //Simply we are implementing the indexing in linked list    
+    getAt = function(index: number): ListNode|null {
+        if(index < 0 || index >= this.size){
+            console.log("Index out of range");
+            return null;
+        }
+
+        let temp = this.head;
+        let currentIndex = 0;
+
+        while(currentIndex != index){
+            currentIndex++;
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+
+    //Add a node at a index 
+    addAt = function(index: number, val: number): void{
+        //invalid case
+        if(index < 0 || index > this.size){
+            console.log("Index out of range");
+            return;
+        }else if(index == 0){
+            this.addFirst(val);
+            return;
+        }else if(index == this.size){
+            this.addLast(val);
+            return;
+        }
+        //add in the middle of the linked list
+        else{
+            let nn = new ListNode(val);
+            let temp = this.getAt(index - 1);
+            nn.next = temp.next;
+            temp.next = nn;
+            this.size++;
+        }
+
+    }
+
+
+    //remove the node from the list
+    removeAt = function(index: number): number {
+        //invalid case
+        if(index < 0 || index >= this.size){
+            console.log("Index out of range");
+            return -1;
+        }//remove first
+        else if(index == 0){
+            return this.removeFirst();
+        }//remove last
+        else if(index == this.size-1){
+            return this.removeLast();
+        }
+
+        else{
+            let temp = this.getAt(index-1)
+            let returnVal = temp.next.val;
+            temp.next = temp.next.next;
+            this.size--;
+            return returnVal;
+        }
+    }
 }
+
 
 let ll = new LinkedList();
 
@@ -77,8 +193,18 @@ ll.addLast(30);
 ll.addLast(20);
 ll.addLast(10);
 
-ll.addFirst(50);
 ll.addFirst(10000);
 
-console.log(ll.getSize());
+
+ll.display();
+console.log("We have removed the first node which is - "+ll.removeFirst());
+ll.display();
+
+console.log(ll.getAt(4).val);
+ll.display();
+
+ll.addAt(2, 5000);
+ll.display();
+
+ll.removeAt(0);
 ll.display();
