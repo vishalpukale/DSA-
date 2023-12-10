d1c72f2785e172bb8a04f774912f1bb9746be732
vishalpukale <vishalpukale12@.com>
2023-12-10 14:32:10 +0530

    10dec

---
+++ 62-30_Oct-2023 Live Class - Linked Lists2/2_middleOfTheLinkedList.ts
@@ -0,0 +1,22 @@
+  class ListNode {
+      val: number
+      next: ListNode | null
+      constructor(val?: number, next?: ListNode | null) {
+          this.val = (val===undefined ? 0 : val)
+         this.next = (next===undefined ? null : next)
+    }
+ }
+
+function middleNode(head: ListNode | null): ListNode | null {
+    
+    let fast =head;
+    let slow =head;
+
+    while(fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    //The above loop will only break if fast becomes null or tail
+
+    return slow;  //the place where slow is standing will be the middle point
+};
