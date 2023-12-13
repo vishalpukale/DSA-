d06c5a0bc772eb0d699d39de0f13934fe185c40d
vishalpukale <vishalpukale12@.com>
2023-12-13 13:06:50 +0530

    stack impliment using array

---
+++ 63-3_Nov-2023 Live Class - Stacks/1_implimentStack.js
@@ -0,0 +1,9 @@
+var stack = [];
+stack.push(10);
+stack.push(20);
+stack.push(30);
+console.log(stack);
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack);
---
+++ 63-3_Nov-2023 Live Class - Stacks/1_implimentStack.ts
@@ -0,0 +1,11 @@
+let stack: number[] = [];
+
+stack.push(10);
+stack.push(20);
+stack.push(30);
+
+console.log(stack);
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack);
